e4dbe1957802885360e172c96616dbcf9f0d1198
¨cosmic-axolotl¨ <cosmic-axolotl@github.com>
2026-03-26 19:29:08 -0300

    feat: frontend React local conectado ao backend

---
+++ astro-query-frontend/src/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8000';
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: 30000,
+});
+
+export async function searchObject(name, options = {}) {
+    const params = {
+        name,
+        include_hipparcos: options.hipparcos ?? true,
+        include_2mass: options.twoMass ?? true,
+        include_ads: options.ads ?? false,
+        ads_limit: options.adsLimit ?? 3,
+    };
+    const response = await api.get('/search/object', { params });
+    return response.data;
+}
+
+export async function searchByType(query, limit = 20) {
+    const response = await api.get('/search/type', { params: { query, limit } });
+    return response.data;
+}
+
+export async function checkHealth() {
+    const response = await api.get('/');
+    return response.data;
+}
